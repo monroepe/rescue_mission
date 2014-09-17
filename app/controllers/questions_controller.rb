e9a50a70023cb4e175b17f45cd4272ea1f956ac4
Peter <monroepe@gmail.com>
2014-09-17 14:28:41 -0400

    Added validations for form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class QuestionsController < ApplicationController
   def index
   end
@@ -5,4 +6,24 @@ class QuestionsController < ApplicationController
   def new
     @question = Question.new
   end
+
+  def create
+    @question = Question.new(question_params)
+
+    respond_to do |format|
+      if @question.save
+        format.html { redirect_to '/questions/new'}
+      else
+        format.html { render action: 'new' }
+      end
+    end
+
+  end
+
+
+private
+    def question_params
+      params.require(:question).permit(:title, :description)
+    end
+
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,5 @@
 class Question < ActiveRecord::Base
+  validates :title, length: { minimum: 40 }
+  validates :description, length: { minimum: 150 }
+  validates :title, :description, presence: true
 end
