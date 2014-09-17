702654aba637a74d765b27e59a22a8cfd0035951
Peter <monroepe@gmail.com>
2014-09-17 12:06:25 -0400

    Form to add new question

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140917155348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: true do |t|
+    t.string   "title",       null: false
+    t.text     "description", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
