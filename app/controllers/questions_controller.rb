9ae1b57e662eb334b6e18d2c32d7f8464e003a0a
Peter <monroepe@gmail.com>
2014-09-18 09:20:53 -0400

    Delete questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -41,6 +41,14 @@ class QuestionsController < ApplicationController
 
   end
 
+  def destroy
+    @question = Question.find(params[:id])
+
+    @question.destroy
+
+    redirect_to questions_path
+  end
+
 
 private
     def question_params
